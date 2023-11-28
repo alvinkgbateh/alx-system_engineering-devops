a6810c450e706156ed34d5d7e1cdff46688587bd
alvinkgbateh <alvingbatehsl@gmail.com>
2023-11-28 14:16:59 -0800

    Updated files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# create a file in /tmp.
+# Create a file in /tmp
 
-file { '/tmp/holberton':
+file { 'school':
   ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+  path    => '/tmp/school',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-#Using Puppet  to, install puppet-lint.
+# Installs puppet-lint
 
 package { 'puppet-lint':
   ensure   => '2.1.1',
